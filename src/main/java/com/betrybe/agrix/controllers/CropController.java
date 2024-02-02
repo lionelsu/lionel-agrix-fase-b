781186e164f835c82f70c7d112dbdd32e3f7ccd8
lionelsu <lionel_suicune@hotmail.com>
2024-02-01 23:12:24 -0400

    feat: Get /crops/search

--- src/main/java/com/betrybe/agrix/controllers/CropController.java
+++ src/main/java/com/betrybe/agrix/controllers/CropController.java
@@ -5,6 +5,7 @@ import com.betrybe.agrix.controllers.dto.FertilizerDto;
 import com.betrybe.agrix.models.entities.Crop;
 import com.betrybe.agrix.models.entities.Fertilizer;
 import com.betrybe.agrix.services.CropService;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -69,5 +71,18 @@ public class CropController {
         .map(FertilizerDto::toDto)
         .toList();
     return ResponseEntity.ok(fertilizerDtos);
+  }
+
+  /**
+   * Search crops response entity.
+   */
+  @GetMapping("/search")
+  public ResponseEntity<List<CropDto>> getCropsByHarvestDay(@RequestParam LocalDate start,
+      @RequestParam LocalDate end) {
+    List<Crop> crops = cropService.findByPlantingDateBetween(start, end);
+    List<CropDto> cropDto = crops.stream()
+        .map(CropDto::toDto)
+        .toList();
+    return ResponseEntity.ok(cropDto);
   }
 }
--- src/main/java/com/betrybe/agrix/services/CropService.java
+++ src/main/java/com/betrybe/agrix/services/CropService.java
@@ -6,6 +6,7 @@ import com.betrybe.agrix.models.entities.Crop;
 import com.betrybe.agrix.models.entities.Fertilizer;
 import com.betrybe.agrix.models.repositories.CropRepository;
 import com.betrybe.agrix.models.repositories.FertilizerRepository;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -67,5 +68,9 @@ public class CropService {
   public List<Fertilizer> getFertilizersByCropId(Long cropId) {
     Crop crop = cropRepository.findById(cropId).orElseThrow(CropNotFoundException::new);
     return crop.getFertilizers();
+  }
+
+  public List<Crop> findByPlantingDateBetween(LocalDate startDate, LocalDate endDate) {
+    return cropRepository.findByHarvestDateBetween(startDate, endDate);
   }
 }
